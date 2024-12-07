2e29c866923dc427b413d5ada55dbd7ad1b73d1e
arunnats <arunnats2004@gmail.com>
2024-12-07 14:07:16 +0530

    hero desktop and tablet done

--- src/components/Common/Button.jsx
+++ src/components/Common/Button.jsx
@@ -10,9 +10,9 @@ const Button = ({ link, bgColor, textColor, children }) => {
         backgroundColor: bgColor,
         color: textColor,
       }}
-      className="w-[18vw] h-[3.9vw] px-1 py-4 rounded-full align-middle font-medium text-[1.8vw] text-center hover:opacity-90 transition-opacity duration-400 "
+      className="w-[30vw] h-[4.1vw] md:w-[18vw] md:h-[3.9vw] px-1 py-4 rounded-full align-middle font-medium text-[3vw] md:text-[1.8vw] text-center hover:opacity-90 transition-opacity duration-400 "
     >
-      <div className="-translate-y-[0.9vw] lg:-translate-y-[0.3vw]">
+      <div className="-translate-y-[1.6vw] md:-translate-y-[0.9vw] lg:-translate-y-[0.3vw]">
         {" "}
         {children}
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       bg-[url('/bg/Hero-Mobile.svg')] 
       sm:bg-[url('/bg/Hero-Tablet.svg')] 
       lg:bg-[url('/bg/Hero-Desktop.svg')]
-      flex flex-col  items-center px-8 lg:px-16"
+      flex flex-col  items-center px-8 lg:px-16 overflow-hidden"
     >
       <div className="navbar font-poppins text-[#FFEFD5] mt-4">
         <div className="navbar-start">
@@ -17,10 +17,9 @@ const Home = () => {
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke="currentColor"
+                className="h-8 w-8 stroke-black"
               >
                 <path
                   strokeLinecap="round"
@@ -69,25 +68,26 @@ const Home = () => {
           </Link>
         </div>
       </div>
-      <div className="flex flex-row items-center w-full space-x-10 my-10">
-        <div className="w-[60%] text-[#FFEFD5] space-y-4 translate-x-6 translate-y-6 lg:translate-y-0">
-          <h3 className="font-caslon text-[16vw] lg:text-[10vw] font-bold leading-none tracking-wider">
+
+      <div className="flex flex-col md:flex-row items-center w-full space-x-10 my-10">
+        <div className="w-full md:w-[60%] text-[#FFEFD5] space-y-4 translate-x-6 translate-y-6 lg:translate-y-0">
+          <h3 className="justify-center md:justify-start font-caslon text-[22vw] md:text-[16vw] lg:text-[10vw] font-bold leading-none tracking-wider">
             SAHITI
           </h3>
-          <p className="font-poppins text-[3vw] lg:text-[1.8vw] max-w-[90%] lg:max-w-[70%] leading-tight lg:text-justify">
+          <p className="font-poppins text-[4.5vw] md:text-[3vw] lg:text-[1.8vw] max-w-[90%] lg:max-w-[70%] leading-tight lg:text-justify">
             Welcome to Sahiti, Kozhikode’s literary gem! Immerse yourself in a
             celebration of words with thought-provoking discussions, captivating
             readings, and engaging workshops.
           </p>
-          <p className="font-poppins text-[3vw] lg:text-[1.8vw] max-w-[80%] lg:max-w-[60%] leading-tight">
+          <p className="font-poppins text-[4.8vw] md:text-[3vw] lg:text-[1.8vw] max-w-[80%] lg:max-w-[60%] leading-tight">
             Join us at Sahiti, Where literature meets inspiration!
           </p>
         </div>
 
-        <div className="w-[40%] flex flex-col items-center space-y-6 translate-y-[40vh] lg:translate-y-16">
-          <div className="flex flex-col items-center space-y-4 scale-[1.2] -translate-x-8 lg:translate-x-0">
+        <div className="w-full md:w-[40%] flex flex-col items-center space-y-6 translate-y-[17vh] md:translate-y-[40vh] lg:translate-y-16">
+          <div className="flex flex-col items-center space-y-4 scale-[1.2] -translate-x-5 md:-translate-x-8 lg:translate-x-0">
             <img
-              className="w-[100%] lg:w-[80%] h-auto"
+              className="w-[50%] md:w-[100%] lg:w-[80%] h-auto"
               src="/logos/logo-orange.png"
               alt="Sahiti Logo"
             />
