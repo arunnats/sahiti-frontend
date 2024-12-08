19d21dd79ba8fba0f1b22e9161d1e246a3670687
Vinit Keshri <vinitkehri4171@gmail.com>
2024-12-09 02:28:00 +0530

    About us Page

--- src/components/Common/Button.jsx
+++ src/components/Common/Button.jsx
@@ -21,13 +21,13 @@ const Button = ({
       }}
       className={`${
         big
-          ? "w-[60vw] h-[12vw] md:w-[18vw] md:h-[3.9vw] text-[5vw] md:text-[1.8vw]"
-          : "w-[30vw] h-[4.1vw] md:w-[18vw] md:h-[3.9vw] text-[3vw] md:text-[1.8vw]"
-      } px-1 py-4 rounded-full align-middle font-medium text-center hover:opacity-90 transition-opacity duration-400 ${
+          ? "p-5 px-5 text-5xl"
+          : "p-3 px-5 text-3xl"
+      } flex items-center justify-center rounded-full font-medium text-center hover:opacity-80 h-full transition-opacity duration-400 ${
         useAlternateStyle ? "border-2" : ""
       }`}
     >
-      <div className="-translate-y-[1.6vw] md:-translate-y-[0.9vw] lg:-translate-y-[0.3vw] font-poppins">
+      <div className="font-poppins">
         {children}
       </div>
     </a>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ import Gallery from "../components/Gallery/Gallery";
 
 const About = () => {
   return (
-    <div className="bg-[#E97451] p-0 m-0 relative overflow-clip h-[200vh]">
+    <div className="bg-[#E97451] p-0 m-0 relative overflow-clip h-max">
       {/* Left  Border */}
       <div className="absolute top-0 -left-14 md:left-0 md:h-screen h-[50%]">
         <img  src="/illustrations/borderTopLeft.svg"  alt="Left Illustration"  className="w-[241px]"/>
@@ -30,14 +30,17 @@ const About = () => {
       >
         <Navbar />
         <h1 className="text-white text-4xl font-bold mt-8">About Us</h1>
-        <p className="text-white text-center mt-4 px-16 md:px-52 text-xl">
-          The Literary and Debating Club of NIT Calicut is one of the oldest and the most prestigious clubs of NITC. Kindled by the passion for the world of literature, the club is a stage for people to to flaunt their belletristic, oratory and expressive genius. Through the years, we have been striving persistently to spread our expense in close accordance with our rule of thumb, 'Deeds not Words'. Organizing competitions in extempore, elocution, debates, quizzes and so much more has helped the club live up to and beyond the expectations for which it was set up.
-            To explore the various ventures of LnD, visit this page:
+        <p className="text-white text-center m-10 px-4 md:px-52 text-xl">
+          <a href="https://www.lndnitc.org/" className="font-bold underline">The Literary and Debating Club</a> of NIT Calicut is one of the oldest and the most prestigious clubs of NITC. 
+          Kindled by the passion for the world of literature, the club is a stage for people to to flaunt their belletristic, oratory and expressive genius. Through the years, we have been striving persistently to spread our expense in close accordance with our rule of thumb, 'Deeds not Words'. Organizing competitions in extempore, elocution, debates, quizzes and so much more has helped the club live up to and beyond the expectations for which it was set up.
+          <br /><br />To explore the various ventures of LnD, visit this page:
 
         </p>
-        <Button link="/#about" bgColor="#FF8343" textColor="#FFFDD0">
-          Learn More
+        <br />
+        <Button link="https://links.lndnitc.org/" bgColor="#FF8343" textColor="#FFFDD0" >
+            links.lndnitc.org
         </Button>
+        <br />
       </div>
 
       {/* Second Section */}
@@ -59,9 +62,12 @@ const About = () => {
 
       <div
           id="gallery"
-          className="w-[90%] min-h-screen bg-[#feefd7] flex justify-center p-4 mx-auto"
+          className=" min-h-screen bg-[#feefd7] flex justify-center mx-[106px] mx-auto"
+          style={{
+            width: "calc('100%-208px')",
+          }}
         >
-          <div className="h-[80%] my-2">
+          <div className="h-[10%] my-2">
             <Gallery />
           </div>
         </div>
