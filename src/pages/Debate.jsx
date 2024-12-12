eb6dc505cda5f7433b067f90ae62a9f68250b4e5
arunnats <arun_b220019cs@nitc.ac.in>
2024-12-12 19:54:42 +0530

    deabte

--- src/pages/Debate.jsx
+++ src/pages/Debate.jsx
@@ -6,103 +6,112 @@ import RightArchBorder from "../components/Common/RightArchBorder";
 import LeftArchBorder from "../components/Common/LeftArchBorder";
 
 const Debate = () => {
-  return (
-    <div className="bg-[#FF8343] p-0 m-0 relative overflow-clip h-max">
-      {/* Left  Border */}
-      <LeftArchBorder />
+	return (
+		<div className="bg-[#FF8343] p-0 m-0 relative overflow-clip h-max">
+			{/* Left  Border */}
+			<LeftArchBorder />
 
-      <RightArchBorder />
+			<RightArchBorder />
 
-      <div
-        id="about"
-        className="min-h-screen flex flex-col items-center px-2 md:px-8 lg:px-16 overflow-hidden relative "
-      >
-        <Navbar />
-        <h1 className="text-white md:text-6xl text-3xl center font-bold mt-8 mx-20 text-center ">Sahiti Parliamentary Debate</h1>
+			<div
+				id="about"
+				className="min-h-screen flex flex-col items-center px-2 md:px-8 lg:px-16 overflow-hidden relative "
+			>
+				<Navbar />
+				<h1 className="text-white md:text-6xl text-3xl center font-bold mt-8 mx-20 text-center ">
+					Sahiti Parliamentary Debate
+				</h1>
 
-        <img className="h-[200px] w-auto mt-6" src="/logos/sahiti-cream.svg" alt="alt" />
-        {/* <img
+				<img
+					className="h-[250px] w-auto mt-6"
+					src="/logos/debate.svg"
+					alt="alt"
+				/>
+				{/* <img
 					className="w-[60vw] ipadAir:w-[65vw] ipadPro:w-[60vw] smallHeight:w-[50vw] md:w-[60vw] lg:w-[25vw] my-3"
 					src="/logos/debate-orange.png"
 					alt="Sahiti Debate"
 				/> */}
 
-      
-        <p className="text-white m-10 mt-7 mb-3 px-4 md:px-52 text-xl text-justify">
-          The flagstone event will be the NITC Parliamentary Debate - held in a BPD format under the WUDC (World Universities Debating Championship) model. The Debate will span 2 days and aims to provide participants with a platform to engage in structured debates, enhancing their public speaking, analytical, and decision-making skills
-          <br />
-          <br />
-          To register for the event, visit :
-        </p>
+				<p className="text-white m-10 mt-1 mb-3 px-4 md:px-52 text-xl text-justify">
+					The flagstone event will be the NITC Parliamentary Debate - held in a
+					BPD format under the WUDC (World Universities Debating Championship)
+					model. The Debate will span 2 days and aims to provide participants
+					with a platform to engage in structured debates, enhancing their
+					public speaking, analytical, and decision-making skills
+				</p>
 
-        <p className="text-white m-10 mt-7 mb-3 px-4 md:px-52 text-xl text-justify">
-          The debate will be done in 2 pools :
-          <br />
-        </p>
+				<h1 className="text-white md:text-4xl text-2xl center font-bold mt-8 mx-20 text-center ">
+					Collegiate Debate
+				</h1>
+				<p className="text-white m-10 mt-7 mb-3 px-4 md:px-52 text-xl text-justify">
+					Battle between the best of the best! The Collegiate Debate will be
+					held in a BPD format under the WUDC (World Universities Debating
+					Championship) model. The Debate will span 2 days and aims to provide
+					participants with a platform to engage in structured debates,
+					enhancing their public speaking, analytical, and decision-making
+					skills.
+					<br />
+				</p>
 
-        <h1 className="text-white md:text-4xl text-2xl center font-bold mt-8 mx-20 text-center ">Collegiate Debate</h1>
-        <p className="text-white m-10 mt-7 mb-3 px-4 md:px-52 text-xl text-justify">
-          Battle between the best of the best! The Collegiate Debate will be held in a BPD format under the WUDC (World Universities Debating Championship) model. The Debate will span 2 days and aims to provide participants with a platform to engage in structured debates, enhancing their public speaking, analytical, and decision-making skills.
-          <br />
-        </p>
+				<br />
 
-        <br />
+				<div className="flex flex-col md:flex-row justify-center items-center space-y-2 md:space-x-4 md:space-y-0 mt-6">
+					<Button
+						link="/brochures/Sahiti_Parliamentary_Debate.pdf"
+						bgColor="#FFFDD0"
+						textColor="#FF8343"
+						isPDF={true}
+					>
+						View Brochure
+					</Button>
+					<Button
+						link="https://docs.google.com/forms/d/e/1FAIpQLSci2XwY2BLfXg76cKGpRZ_88dmbxmWtUZoGcrCkJ9kPGUqk7A/viewform"
+						bgColor="#FFFDD0"
+						textColor="#FF8343"
+					>
+						Register Now
+					</Button>
+				</div>
 
-        <div className="flex flex-col md:flex-row justify-center items-center space-y-2 md:space-x-4 md:space-y-0 mt-6">
-          <Button
-            link="/brochures/Sahiti-Brochure.pdf"
-            bgColor="#FFFDD0"
-            textColor="#FF8343"
-            isPDF={true}
-          >
-            View Brochure
-          </Button>
-          <Button
+				<h1 className="text-white md:text-4xl text-2xl center font-bold mt-8 mx-20 text-center ">
+					Scholastic Debate
+				</h1>
+				<p className="text-white m-10 mt-7 mb-3 px-4 md:px-52 text-xl text-justify">
+					The Scholastic Debate will be held in a BPD format under the WUDC
+					(World Universities Debating Championship) model. The Debate will span
+					2 days and aims to provide participants with a platform to engage in
+					structured debates, enhancing their public speaking, analytical, and
+					decision-making skills.
+					<br />
+				</p>
+
+				<br />
+
+				<div className="flex flex-col md:flex-row justify-center items-center space-y-2 md:space-x-4 md:space-y-0 mt-6">
+					<Button
+						link="/brochures/Sahiti_Parliamentary_Debate.pdf"
+						bgColor="#FFFDD0"
+						textColor="#FF8343"
+						isPDF={true}
+					>
+						View Brochure
+					</Button>
+					{/* <Button
           link="https://docs.google.com/forms/d/e/1FAIpQLSci2XwY2BLfXg76cKGpRZ_88dmbxmWtUZoGcrCkJ9kPGUqk7A/viewform"
           bgColor="#FFFDD0"
           textColor="#FF8343"
         >
           Register Now
-        </Button>
-        </div>
+        </Button> */}
+				</div>
 
-        
-        <h1 className="text-white md:text-4xl text-2xl center font-bold mt-8 mx-20 text-center ">Scholastic Debate</h1>
-        <p className="text-white m-10 mt-7 mb-3 px-4 md:px-52 text-xl text-justify">
-          The Scholastic Debate will be held in a BPD format under the WUDC (World Universities Debating Championship) model. The Debate will span 2 days and aims to provide participants with a platform to engage in structured debates, enhancing their public speaking, analytical, and decision-making skills.
-          <br />
-        </p>
+				<br />
+				<br />
+			</div>
 
-        
-
-        <br />
-
-        <div className="flex flex-col md:flex-row justify-center items-center space-y-2 md:space-x-4 md:space-y-0 mt-6">
-          <Button
-            link="/brochures/Sahiti-Brochure.pdf"
-            bgColor="#FFFDD0"
-            textColor="#FF8343"
-            isPDF={true}
-          >
-            View Brochure
-          </Button>
-          <Button
-          link="https://docs.google.com/forms/d/e/1FAIpQLSci2XwY2BLfXg76cKGpRZ_88dmbxmWtUZoGcrCkJ9kPGUqk7A/viewform"
-          bgColor="#FFFDD0"
-          textColor="#FF8343"
-        >
-          Register Now
-        </Button>
-        </div>
-
-        <br /><br />
-        
-
-        
-      </div>
-
-      {/* Second Section */}
-      {/* <div className="absolute top-screen z-10 -left-14 md:left-0 h-screen rotate-180">
+			{/* Second Section */}
+			{/* <div className="absolute top-screen z-10 -left-14 md:left-0 h-screen rotate-180">
         <img
           src="/illustrations/borderTopRight.svg"
           alt="Left Illustration for Second Section"
@@ -117,8 +126,8 @@ const Debate = () => {
           className="h-full"
         />
       </div> */}
-    </div>
-  );
+		</div>
+	);
 };
 
 export default Debate;
